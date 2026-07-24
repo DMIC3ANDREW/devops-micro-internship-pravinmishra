79ddd7d4e2e00f5382c57d51e13b26c1aa98b3f8
Andrew Ogunlana <your-email@example.com>
2026-07-24 12:47:34 +0000

    Add React app source files (Assignment 2)

---
+++ my-react-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <p>Deployed by Andrew Ogunlana on 24 July 2026</p>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
